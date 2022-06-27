85dc187f70f2e9b112bbd141c45fd84ee7059821
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2022-06-27 12:08:57 -0500

    metodo get

--- index.js
+++ index.js
@@ -3,8 +3,50 @@ const app = express();
 const port = 3000;
 
 app.get ('/', (req, res)=>{
- res.send('hola respuesta');
-})
+  res.send('hola respuesta');
+  });
+
+  app.get ('/inicio', (req, res)=>{
+    res.send('hola desde el inicio');
+  });
+
+  app.get ('/autos', (req, res)=>{
+    res.json([
+      {
+        name: 'coche',
+      precio: 68900
+      },
+      {
+        name: 'coche2',
+      precio: 38900
+      },
+    ]
+
+    );
+  });
+  app.get ('/autos/:id', (req, res)=>{
+    const {id} = req.params;
+
+    res.json(
+      {
+        id,
+        name: 'coche',
+        precio: 68900
+      },
+    );
+  });
+
+  app.get ('/autos/:autId/categoria/:catId', (req, res)=>{
+    const {autId, catId} = req.params;
+
+    res.json(
+      {
+        autId,
+        catId,
+      },
+    );
+  });
+
 
 app.listen(port, ()=>{
   console.log("mi aplicacion" + port);
